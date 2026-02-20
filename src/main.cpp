5bdc809e1316a78704991ecd24f3b7108bb455c4
Kunal Gupta <kunal.gupta_cs.aiml24@gla.ac.in>
2026-02-20 15:19:06 +0530

    Update author information in main.cpp
    Updated author name and date in the main.cpp file.

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,15 @@
 /**
  * @file main.ino
  * @brief Embedded Light Intensity Monitoring using LDR
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Kunal Gupta
+ * @date 2026-02-20
  *
  * @details
  * Reads analog brightness data from LDR sensor
  * and displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define LDR analog pin (Use A0)
-
- // TODO 2:
- // Create variable to store sensor reading
+ 
 
 void setup() {
 
